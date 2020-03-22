dc8241fa965cba453ed989cd4fe8cae051c34d05
mpsci-iscteiul <mpsci@iscte-iul.pt>
2020-03-22 16:40:24 +0000

    yauuu

---
+++ Teste/src/TestesUnitarios/Teste_TesteClasse.java
@@ -0,0 +1,14 @@
+package TestesUnitarios;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Teste_TesteClasse {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
