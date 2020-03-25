c2ca62c138daf3cee874752db0b680125f28c759
mpsci-iscteiul <mpsci@iscte-iul.pt>
2020-03-25 17:53:24 +0000

    commit 17:53

---
+++ HelloWorld/src/test/java/ESII/HelloWorld/AllTests.java
@@ -0,0 +1,11 @@
+package ESII.HelloWorld;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AppTest.class, G14Test.class })
+public class AllTests {
+
+}
